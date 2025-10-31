0d15913b7b995dad72d181665eeba696efef6134
wesley <wpkpoya@gmail.com>
2025-10-31 22:41:40 +0100

    j'ai commencer la partie messages

---
+++ web/src/messag.jsx
@@ -0,0 +1,27 @@
+import './message.css'
+function Messag(){
+    return( 
+        // le container qui regroupe tous
+        <div className="container">
+            <div className='clav'>
+
+            </div>
+            <div className='user'>
+
+            </div>
+            <div className='detail'>
+
+            </div>
+             <div className='mess'>
+                <div className='A'>
+                     <h2>Message</h2>
+                     <button>...</button>
+                </div>
+            </div>
+            <div className='Me'>
+
+            </div>
+        </div>
+    )       
+}
+export default Messag
